501878e48b208c0c07e4a67c70334a5d9514bb1f
Justin Roberts <justin.roberts2@ge.com>
2015-08-11 16:22:22 -0400

    adding remote command script

---
+++ remote_commands/remotecmd.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+#$mount="$ARGV[0]\n";
+#chomp($mount);
+$user=<username>
+$hostlist="<hostlist file>";
+unless (-e $hostlist){
+        print "No hostlist file present!!!\n";
+        exit;
+}
+$cmd="<commands to run>";
+open(HOSTLIST,$hostlist);
+        while(<HOSTLIST>) {
+                $output="";
+                @split="";
+                chomp;
+                $hostname=$_;
+                $sshcmd="ssh -o ConnectTimeout=2 -o BatchMode=yes -o StrictHostKeyChecking=no -naxt $user\@$hostname \"($cmd)\" 2>&1";
+                $output=`$sshcmd`;
+                chomp($output);
+                print $hostname . " - " . $output . "\n";
+        }
+close(HOSTLIST);
