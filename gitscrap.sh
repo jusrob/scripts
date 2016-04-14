aba0e93685398c736a3b8f1c0833b8dae7f807ef
Justin Matthew Roberts <jm.roberts@outlook.com>
2016-04-14 10:16:43 -0400

    Update gitscrap.sh

--- gitscrap.sh
+++ gitscrap.sh
@@ -1,1 +1,3 @@
 curl -u jusrob -s https://api.github.com/orgs/[ORG]/repos?per_page=500 | ruby -rubygems -e 'require "json"; JSON.load(STDIN.read).each { |repo| %x[git clone #{repo["clone_url"]} ]}'
+
+curl -u jusrob -s https://github.com/api/v3/orgs/[ORG]/repos?per_page=500 | ruby -rubygems -e 'require "json"; JSON.load(STDIN.read).each { |repo| %x[git clone #{repo["clone_url"]} ]}'
